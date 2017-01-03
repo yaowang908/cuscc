e78ae0e71352092c012594fef1351a4d97dcb340
Yao Wang <yao422321065@yahoo.com>
2017-01-03 11:10:37 -0500

    outdated init

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*  footer template for twentyfifteen 
+*/?>
+<div id="cuscc_footer">
+	<div id="cuscc_footer_margin_helper"></div>
+	<div id="cuscc_contact" class="left">
+		<h2>Contact</h2>
+		<p>212-809-8188</p>
+		<p>China-U.S. Chamber of Commerce</p>
+		<p>1271 Avenue of the Americas Suite 4300 New York NY 10020</p>
+	</div>
+	<div id="cuscc_outter_links" class="left">
+		<h2>Links</h2>
+		<ul>
+			<li><a href="http://www.mygcapitalus.com" target="_blank">MYG Capital</a></li>
+			<li><a href="http://meiyingtzzx.com/" target="_blank">Mei Ying Investment Consulting</a></li>
+			<li><a href="http://www.cuccbrc.com/" target="_blank">CUCC Business Regional Center</a></li>
+			<li><a href="http://www.aurinewyork.com/" target="_blank">Auri International Consulting</a></li>
+			<li><a href="http://www.cnusaedu.com/" target="_blank">Auri International Education</a></li>
+			<li><a href="http://www.heinlyn.com/" target="_blank">Heinlyn Public Relations</a></li>
+					
+		</ul>
+	</div>
+	<div id="cuscc_newsletter_signup" class="left">
+		<h2>Sign up for our newsletter</h2>
+		<input type="email" name="email" id="cuscc_newsletter_signup_email"/>
+		<input type="button" name="submit" id="cuscc_newsletter_signup_submit" value="Subscribe"/>
+		<div class="clear"></div>
+	</div>
+	<div id="cuscc_copyright">
+		<p>&copy; <?php echo date("Y") ?> China-U.S. Chamber of Commerce</p>
+		<p>Transparency & Disclosure | Legal | Privacy</p>
+		<p>美中国际商会|美国商会|中美商会</p>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* twentyseventeen child functions and definitions
+*/
+function twentyseventeen_child_enqueue_syles(){
+	wp_enqueue_style('parent-style',get_template_directory_uri().'/style.css');
+}
+add_action('wp_enqueue_scripts','twentyseventeen_child_enqueue_styles');
+
+
+?>
